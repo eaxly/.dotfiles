fd45d5e613afd160d326e771170fd34812fd3728
Samuel K <mrsosaki@posteo.net>
2021-08-02 11:02:14 +0200

    linux.sh: fix and alter packages

--- linux.sh
+++ linux.sh
@@ -6,46 +6,44 @@
 
 # If you just wan't to install other packages, edit these two lines
 # ↓ packages that need to be installed
-packages="neovim ranger rofi picom alacritty"
+packages="neovim alacritty kitty gnome"
 
-aur_packages="neovim-git picom-jonaburg-gt"
+aur_packages=""
 
-_exists() { if command -v pacman &> /dev/null; then
-    pacman -Q ${@} &> /dev/null
-  elif command -v apt &> /dev/null; then
-    apt search ${@} &> /dev/null
-  fi
+_exists() {
+    if command -v pacman &> /dev/null; then
+        pacman -Q ${@} &> /dev/null
+    fi
 }
 
 _install() {
-  program=${1}
-  if _exists pacman; then
-    sudo pacman -S $program
-  elif _exists apt; then
-    sudo apt install $program
-  fi
+    program=${@}
+    echo -e "Installing ${program}..."
+    if _exists pacman; then
+        sudo pacman -S $program
+    fi
 }
 
 _aur_install() {
-  program=${1}
-  if ! _exists $program; then
-    sudo paru -S $program
-  fi
+    program=${1}
+    if ! _exists $program; then
+        paru -S $program
+    fi
 }
 
 if _exists pacman && ! _exists yay && ! _exists paru && ! _exists yoghurt; then
-  echo "You have no AUR manager installed on your system."
-  echo "paru is an AUR helper written in Rust and an alternative to yay"
-  echo "Do you wan't to install paru? [y/N]"
-  read answer
-  if [[ ${answer,,} == "n" || ${answer,,} == "no" ]]; then
-    echo "OK! Skipping..."
-  else
+    echo "You have no AUR manager installed on your system."
+    echo "paru is an AUR helper written in Rust and an alternative to yay"
+    echo "Do you wan't to install paru? [y/N]"
+    read answer
+    if [[ ${answer,,} == "n" || ${answer,,} == "no" ]]; then
+        echo "OK! Skipping..."
+    else
     echo Cloning...
     git clone https://aur.archlinux.org/paru-bin.git $HOME/paru-bin
     sleep 1
-    
-    echo Going into the directory...
+
+    echo Entering the home directory...
     cd $HOME/paru-bin
     sleep 1
 
@@ -53,11 +51,14 @@ if _exists pacman && ! _exists yay && ! _exists paru && ! _exists yoghurt; then
     makepkg -si
     sleep 1
     echo DONE!
-  fi
+    fi
 fi
 
-_install $package
+if $packages != ""; then
+    _install $packages
+fi
 
-_aur_install $package
-
+if $aur_packages != ""; then
+    _aur_install $packages
+fi
 # vim:foldmethod=marker
