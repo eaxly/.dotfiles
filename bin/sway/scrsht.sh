10cbde5776f4aeab9158191c63bf796a7fc9a58f
Samuel K <mrsosaki@posteo.net>
2022-02-05 10:00:50 +0100

    feat(sway): screenshot script

---
+++ bin/sway/scrsht.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+# by @ExtinctAxolotl
+screen_dir="$(xdg-user-dir PICTURES)/Screenshots"
+screen_path="$screen_dir/$(date +'%Y-%m-%d_%X_grim.png')"
+output="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')"
+if [ ! -d "$screen_dir" ]; then
+    mkdir -p $screen_dir
+fi
+
+usage() {
+    echo "
+    $(basename $0) - make screenshots with style
+    -c, --copy - copy screenshot
+    -s, --selection - selection shot
+    -f, --full - full shot
+    "
+}
+
+while [ $# -ge 0 ]; do
+    case "$1" in
+        -c | --copy)
+            copy=true
+            shift
+            ;;
+        -s | --selection)
+            selection=true
+            shift
+            ;;
+        -f | --full)
+            full=true
+            shift
+            ;;
+        -*)
+            usage
+            exit
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+selection_shot() {
+    if [ ! "$copy" = true ]; then
+        grim -g "$(slurp)" $screen_path
+        stat="$?"
+    else
+        grim -g "$(slurp)" - | wl-copy -t image/png
+        stat="$?"
+    fi
+    echo "copy: $copy $screen_path"
+}
+
+full_shot() {
+    if [ ! "$copy" = true ]; then
+        grim -o "$output" $screen_path
+        stat="$?"
+    else
+        grim -o "$output" - | wl-copy -t image/png
+        stat="$?"
+    fi
+    echo "copy: $copy $screen_path"
+}
+
+if [ "$full" = true ] && [ ! "$selection" = true ]; then
+    full_shot
+elif [ ! "$full" = true ] && [ "$selection" = true ]; then
+    selection_shot
+else
+    echo nope
+fi
+
+if [ "$stat" = "0" ]; then
+    [ "$copy" = true ] && wl-paste > /tmp/scrsht && image="/tmp/scrsht" || image="$screen_path"
+    dunstify --raw_icon $image "Screenshot taken!" "Copied: $copy"
+fi
