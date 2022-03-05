23f45a023b067a197620cf9a17003504640034d6
Samuel K <mrsosaki@posteo.net>
2022-03-05 22:04:59 +0100

    remove bin/i3

--- bin/i3/exit-i3.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-zenity --title=i3 --question --text="Exit i3?"
-
-if [ $? == 0 ]; then
-    i3-msg exit
-fi
--- bin/i3/keyring.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$DESKTOP_SESSION" ];then
-    eval $(gnome-keyring-daemon --start --components=ssh,secrets,pkcs11)
-    export SSH_AUTH_SOCK
-fi
