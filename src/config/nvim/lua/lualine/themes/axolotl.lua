06612de512f223c53566015b865b0f6b8cdcc683
Samuel K <mrsosaki@posteo.net>
2022-04-24 21:34:43 +0100

    feat(nvim): remove bold font in lualine
    lualine.nvim now has no bold fonts in it.

--- src/config/nvim/lua/lualine/themes/axolotl.lua
+++ src/config/nvim/lua/lualine/themes/axolotl.lua
@@ -5,32 +5,32 @@ end
 
 return {
   normal = {
-    a = { bg = c.green, fg = c.bg, gui = "bold" },
+    a = { bg = c.green, fg = c.bg },
     b = { bg = c.bg_alt, fg = c.fg },
     c = { bg = c.bg, fg = c.fg_alter },
   },
   insert = {
-    a = { bg = c.blue, fg = c.bg, gui = "bold" },
+    a = { bg = c.blue, fg = c.bg },
     b = { bg = c.bg_alt, fg = c.fg },
     c = { bg = c.bg, fg = c.fg_alter },
   },
   visual = {
-    a = { bg = c.magenta, fg = c.bg, gui = "bold" },
+    a = { bg = c.magenta, fg = c.bg },
     b = { bg = c.bg_alt, fg = c.fg },
     c = { bg = c.bg, fg = c.fg_alter },
   },
   replace = {
-    a = { bg = c.red, fg = c.bg, gui = "bold" },
+    a = { bg = c.red, fg = c.bg },
     b = { bg = c.bg_alt, fg = c.fg },
     c = { bg = c.bg, fg = c.fg_alter },
   },
   command = {
-    a = { bg = c.yellow, fg = c.bg, gui = "bold" },
+    a = { bg = c.yellow, fg = c.bg },
     b = { bg = c.bg_alt, fg = c.fg },
     c = { bg = c.bg, fg = c.fg_alter },
   },
   inactive = {
-    a = { bg = c.bg, fg = c.black_br, gui = "bold" },
+    a = { bg = c.bg, fg = c.black_br },
     b = { bg = c.bg, fg = c.black_br },
     c = { bg = c.bg, fg = c.black_br },
   },
