e2c510d5fc586504e9c8d33f8351e826e79f4ad7
Samuel K <mrsosaki@posteo.net>
2022-04-27 11:07:56 +0100

    chore(nvim): nvim-tree configuration
    The nvim tree configuration now is more lua.

--- src/config/nvim/lua/configs/filetree.lua
+++ src/config/nvim/lua/configs/filetree.lua
@@ -3,26 +3,62 @@ if not present then
   print("nvimtree could not load")
   return
 end
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-vim.g.nvim_tree_respect_buf_cwd = 0
 vim.g.nvim_tree_root_folder_modifier = table.concat({ ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" })
-
 nvimtree.setup({
+  auto_reload_on_write = true,
   disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "alpha" },
-  auto_close = false,
-  open_on_tab = false,
   hijack_cursor = true,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = true,
+  ignore_buffer_on_setup = false,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  open_on_tab = false,
+  sort_by = "name",
   update_cwd = true,
-  update_to_buf_dir = {
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    mappings = {
+      custom_only = false,
+      list = {
+        -- user mappings go here
+      },
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+      icons = { corner = "╰ ", edge = "│ ", none = "  " },
+    },
+    icons = {
+      webdev_colors = true,
+    },
+  },
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
+  update_focused_file = {
+    enable = false,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {},
+  system_open = {
+    cmd = "",
+    args = {},
+  },
   diagnostics = {
     enable = true,
+    show_on_dirs = false,
     icons = {
       hint = "",
       info = "",
@@ -30,37 +66,22 @@ nvimtree.setup({
       error = "",
     },
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
   filters = {
     dotfiles = false,
     custom = {},
+    exclude = {},
   },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = "d", cb = tree_cb("trash") },
-        { key = "D", cb = tree_cb("remove") },
-      },
-    },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 400,
   },
   actions = {
+    use_system_clipboard = true,
     change_dir = {
       enable = true,
       global = false,
+      restrict_above_cwd = false,
     },
     open_file = {
       quit_on_open = false,
@@ -75,10 +96,20 @@ nvimtree.setup({
       },
     },
   },
-  renderer = {
-    indent_markers = {
-      enable = true,
-      icons = { corner = "╰ ", edge = "│ ", none = "  " },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
     },
   },
 })
